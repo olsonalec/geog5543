16c318c5221c66ff6cef8af747bf86c06405cc4d
Alec Olson <ols00160@umn.edu>
2024-10-05 18:31:27 -0500

    Added humidity to similarity score

--- mvp1/README.md
+++ mvp1/README.md
@@ -4,14 +4,20 @@
 The final iteration of this project is a program that will tell the user what city on Earth currently has the most similar weather conditions compared to the user's location.
 
 ## Solution
-The user will enter the name of their current city, and then the program will gather real-time weather data from thousands of cities around the globe. This data will include air temperature, wind speed, relative humidity, precipitation, and possibly other things. The program will generate a score for each city, with a higher score indicating that the city's weather is more similar to the user's city. Then, the program will return the name of the city with the highest score, as well as the current weather conditions in both cities.
+The user will enter the name of their current city, and then the program will gather real-time weather data from thousands of cities around the globe. This data will include air temperature, wind speed, relative humidity, precipitation, and possibly other things. The program will generate a score for each city, with a lower score indicating that the city's weather is more similar to the user's city. Then, the program will return the name of the city with the lowest score, as well as the current weather conditions in both cities.
 
 ## Challenges
 1. Logistical: It will be logistically difficult to gather real-time weather data from locations around the world. The program will need to interact with many different governmental agencies' APIs (for example, NOAA in the U.S. and ECMWF in Europe) in order to build a representative sample of global weather conditions.
 2. Scientific: How should the program quantify different aspects of weather? How should it produce a single score that combines many different aspects of weather? For example, how does a 10 degree difference in temperature compare to a 10 mph difference in wind speed? What units should be used? Even within the same unit system, there are different standards of measurement. For example, m/s and km/h are both common measurements of wind speed in the metric system.
 
 ## My MVP
-My MVP features many simplifications compared to the final product. First of all, my MVP will use Minneapolis as the starting city and will only compare cities within the U.S. This allows me to only worry about gathering data from one source: the NOAA. 
+My MVP features many simplifications compared to the final product. First of all, my MVP will use Minneapolis as the starting city and will only compare cities within the U.S. This allows me to only worry about gathering data from one source: the NOAA. Additionally, I will only consider air temperature and relative humidity when calculating a 'similarity score' for each location. The location with the lowest score will be considered as having the most similar weather compared with Minneapolis. This score is calculated as follows:
+
+```
+score = abs(MplsTemp - LocationTemp) + abs(MplsHumidity - LocationHumidity) / 2
+```
+
+Humidity receives only half the weight that air temperature does because I believe that small differences in air temperature are more noticeable than small differences in humidity. There is no scientific evidence to this claim. For example, if one location is 10 degrees cooler than Minneapolis but has the same relative humidity, that weather will feel much different than another location that has the same temperature as Minneapolis but has relative humidity that is 10% lower.
 
 ## The Program
 `mvp1.py` features all the code for this MVP. It is divided into multiple sections.
--- mvp1/mvp1.py
+++ mvp1/mvp1.py
@@ -15,7 +15,7 @@ Get the current temperature at the MSP airport and print this temp to the termin
 mpls = requests.get('https://api.weather.gov/stations/KMSP/observations/latest')
 mpls = mpls.json()
 mplsTemp = mpls['properties']['temperature']['value']
-print(f'Mpls Temp: {mplsTemp}')
+mplsHumidity = mpls['properties']['relativeHumidity']['value']
 
 '''
 Section 2
@@ -38,41 +38,78 @@ stations = list(set(stations))
 
 '''
 Section 3
-Iterate through all the Observation Stations and, if they return a valid response (200), grab the current temperature.
+Iterate through all the Observation Stations and, if they return a valid response (200), grab the current temperature and relative humidity.
+Store all these values in a list [stationID, temp, relativeHumidity], and then store all these lists in stationWeatherList.
 '''
-stationTempList = []
-print(f'There are {len(stations)} stations to compare.')
-for station in stations:
+stationWeatherList = []
+stationsToCompare = stations[:]     # if we want, we can compare only a subset of the total stations
+
+print(f'\nThere are {len(stationsToCompare)} stations in the U.S. to compare.\n')
+
+for station in stationsToCompare:
     weather = requests.get(f'https://api.weather.gov/stations/{station}/observations/latest')
     if weather.status_code == 200:
         weather = weather.json()
         temp = weather['properties']['temperature']['value']
-        if temp != None:
-            stationTempList.append([station, temp])
+        relativeHumidity = weather['properties']['relativeHumidity']['value']
+        weatherList = [station, temp, relativeHumidity]
+        validData = True
+        for item in weatherList:
+            if item == None:
+                validData = False
+        if validData == True:
+            stationWeatherList.append(weatherList)
+
 
 '''
 Section 4
-Find the Observation Station that has a temperature most similar to MSP airport.
+Find the Observation Station that has a temperature and relatively humidity most similar to the MSP airport.
 '''
-closestTemp = 1000
-closestTempDiff = 1000
-closestStation = ''
-for station in stationTempList:
-    tempDiff = mplsTemp - station[1]
-    if abs(tempDiff) < abs(closestTempDiff):
-        closestStation = station[0]
-        closestTemp = station[1]
-        closestTempDiff = tempDiff
+for station in stationWeatherList:
+
+    # calculate absolute difference in temp
+    tempDiff = abs(mplsTemp - station[1])
+
+    # calculate absolute difference in humidity - this is given half the weight that temp is given
+    humidityDiff = abs(mplsHumidity - station[2]) / 2
+
+    # calculate the total score for the station
+    totalScore = tempDiff + humidityDiff
+    station.insert(0, totalScore)
+
+# sort the list of weather stations so that the most similar station is at the front of the list
+stationWeatherList.sort()
+
+# '''
+# Section 4
+# Find the Observation Station that has a temperature most similar to MSP airport.
+# '''
+# closestTemp = 1000
+# closestTempDiff = 1000
+# closestStation = ''
+# for station in stationWeatherList:
+#     # calculate absolute difference in temp
+#     tempDiff = mplsTemp - station[2]
+#     if abs(tempDiff) < abs(closestTempDiff):
+#         closestStation = station[1]
+#         closestTemp = station[2]
+#         closestTempDiff = tempDiff
         
 '''
 Section 5
 Find the name of the location where the Observation Station is located.
 '''
-closestStationName = requests.get(f'https://api.weather.gov/stations/{closestStation}').json()
+closestStation = stationWeatherList[0]
+closestStationID = closestStation[1]
+closestStationTemp = closestStation[2]
+closestStationHumidity = closestStation[3]
+
+closestStationName = requests.get(f'https://api.weather.gov/stations/{closestStationID}').json()
 closestStationName = closestStationName['properties']['name']
-print(f'The current temperature at {closestStationName} is {closestTemp} degrees Celsius.')
-print(f'The current temperature at MSP airport is {mplsTemp} degrees Celsius.')
+print(f'The current weather at MSP airport is {mplsTemp} degrees Celsius with {round(mplsHumidity)}% humidity.\n')
+print(f'The location with the most similar current weather is {closestStationName}.\n')
+print(f'The current weather at {closestStationName} is {closestStationTemp} degrees Celsius with {round(closestStationHumidity)}% humidity.\n')
 
 
 end_time = time.time()
-print(f'The total time to compare 100 cities was {end_time - start_time} seconds.')
+print(f'The total time to compare {len(stationsToCompare)} cities was {end_time - start_time} seconds.\n')
